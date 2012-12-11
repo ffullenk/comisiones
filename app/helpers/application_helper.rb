73c8a8b9a4e8c5ed15394e2f5fa2138ab0b53a8e
Francisco <francisco000@gmail.com>
2012-12-11 01:45:17 -0300

    catalogo con masthead

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+ def usuario_signed_in?
+ 	
+ 	return (vendedor_signed_in? && empresa_signed_in?)
+
+ end
+
+
+ def render_masthead?
+
+ 	controllers = ["home", "sessions",  "registrations", "passwords"]
+
+ return (controllers.include? (controller_name) ||  ((controller_name.eql? "productos") && !usuario_signed_in?))
+ end
+
+
 end
