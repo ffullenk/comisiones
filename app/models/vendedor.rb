f505aca81c22cccbb9cbb1a21fa41eb71933f904
Francisco <francisco000@gmail.com>
2013-01-30 21:30:55 -0300

    fix prints

--- app/models/vendedor.rb
+++ app/models/vendedor.rb
@@ -14,7 +14,7 @@ class Vendedor < ActiveRecord::Base
   has_many :empresas, :through => :assignments
   has_many :catalogos
   validates :email, :uniqueness => true, :format => { :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i }
-  validates :rut, :uniqueness => true, :format => { :with => /\A(\d{1,3})\.(\d{1,3})\.(\d{1,3})\-(k|\d{1})\Z/i ,:message=>"Rut inválido"}
+  validates_uniqueness_of :rut, :format => { :with => /\A(\d{1,3})\.(\d{1,3})\.(\d{1,3})\-(k|\d{1})\Z/i ,:message=>"Rut inválido"}
                       
   belongs_to :comuna
   belongs_to :universidad
